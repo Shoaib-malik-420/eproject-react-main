a22c6d02964301ee2ec69ce0bf41447608103b41
Shoaib-malik-420 <101936344+Shoaib-malik-420@users.noreply.github.com>
2024-08-01 20:26:22 +0500

    cards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,46 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { HiMiniUserGroup } from "react-icons/hi2";
+import { FaRegUser } from "react-icons/fa";
+
 
 export default function Dashboard() {
   return (
     <>
     
-    
    
  
- <div class="flex flex-row space-x-4 mt-9 pl-56">
-    <div class="flex flex-col justify-between p-4 leading-normal bg-white rounded-lg shadow-md w-80">
-    <div class="flex justify-center items-center mb-4">
-        <HiMiniUserGroup size={80} className='' />
-    </div>
-        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    </div>
+    <div class="flex w-full flex-row space-x-4 mt-9 px-10">
+    <div class=" h-70 p-6 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <h5 class="mb-2 text-xl font-semibold tracking-tight  text-gray-500 dark:text-gray-400 text-left">Request</h5>
+        </a>
+        <div class="flex-row flex justify-between">
+            <p class="font-normal text-black text-4xl font-semibold">4</p>
+            <div className='bg-orange-600 rounded-xl p-4 text-white'><FaRegUser size="28" /></div>
+        </div>
+        <h5 class="text-md font-semibold tracking-tight  text-gray-500 dark:text-gray-400 text-left">Total Request</h5>
 
-    <div class="flex flex-col justify-between p-4 leading-normal bg-white rounded-lg shadow-md w-80">
-    <div class="flex justify-center items-center mb-4">
-        <HiMiniUserGroup size={80} className='' />
-    </div>
-    <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-</div>
-
-
-    
-</div>
-
-<div className='flex flex-row pl-96 mt-4'>
-<div class="flex flex-col justify-between p-4 leading-normal bg-white rounded-lg shadow-md w-80">
-    <div class="flex justify-center items-center mb-4">
-        <HiMiniUserGroup size={80} className='' />
-    </div>
-        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
     </div>
 
-</div>
+    <div class="h-70 p-6 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <h5 class="mb-2 text-xl font-semibold tracking-tight  text-gray-500 dark:text-gray-400 text-left">Consignment</h5>
+        </a>
+        <div class="flex-row flex justify-between">
+            <p class="font-normal text-black text-4xl font-semibold">1</p>
+            <div className='bg-orange-600 rounded-xl p-4 text-white'><FaRegUser size="28" /></div>
+        </div>
+        <h5 class="text-md font-semibold tracking-tight  text-gray-500 dark:text-gray-400 text-left">Total Consignment</h5>
 
+    </div>
+
+
+    <div class="h-70 p-6 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+            <h5 class="mb-2 text-xl font-semibold tracking-tight  text-gray-500 dark:text-gray-400 text-left">Users</h5>
+        </a>
+        <div class="flex-row flex justify-between">
+            <p class=" font-normal text-black text-4xl font-semibold">2</p>
+            <div className='bg-orange-600 rounded-xl p-4 text-white'><FaRegUser size="28" /></div>
+        </div>
+        <h5 class="text-md font-semibold tracking-tight  text-gray-500 dark:text-gray-400 text-left">Total User</h5>
+
+    </div>
+
+            </div>
     </>
   )
 }
